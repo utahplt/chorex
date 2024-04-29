ebcd74b8db065bad45bc4485bce4675e2226a4c7
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-04-29 16:49:35 -0600

    Scaffold out example in docs, take first halting steps to projection

--- lib/chorex.ex
+++ lib/chorex.ex
@@ -1,18 +1,72 @@
 defmodule Chorex do
   @moduledoc """
-  Documentation for `Chorex`.
+  Make your modules dance.
+
+  ```elixir
+  defchor ThreePartySeller(Buyer1, Buyer2, Seller) do
+    Buyer1.get_book_title() -> Seller.b
+    Seller.get_price(b) -> Buyer1.p
+    Seller.get_price(b) -> Buyer2.p
+    Buyer2.(p/2) -> Buyer1.contrib
+
+    if Buyer1.(p - contrib < budget) do
+      Buyer1[Buy] -> Seller
+      Buyer1.address -> Seller.addr
+      Seller.get_delivery(b, addr) -> Buyer1.d_date
+      Buyer1.return(d_date)
+    else
+      Buyer1[NoBuy] -> Seller
+      Buyer1.return(nil)
+    end
+  end
+  ```
+
+  Elsewhere, use like so:
+
+  ```elixir
+  defmodule Buyer1 do
+    use ThreePartySeller, Buyer1
+
+    @impl true
+    def get_book_title(), do: ...
+
+    ...
+  end
+
+  defmodule Seller do
+    use ThreePartySeller, Seller
+
+    @impl true
+    def get_price(book_name), do: ...
+
+    ...
+  end
+
+  defmodule Buyer2 do
+    use ThreePartySeller, Buyer2
+  end
+  ```
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Chorex.hello()
-      :world
-
+  Define a new choreography.
   """
-  def hello do
-    :world
+  defmacro defchor({chor_name, _meta, arglist}, do: block) do
+    # Am I stripping off all the hygiene mechanisms here? It'd be
+    # awesome if we can ensure that the names provided by the user are
+    # hygienic so that a choreography can compose with other
+    # metaprogrammings!
+    actors = for {:__aliases__, _meta, [name]} <- arglist, do: name
+    IO.inspect(actors, label: "actors")
+    quote do
+      42
+    end
+  end
+
+  @doc """
+  Perform endpoint projection in the context of node `label`.
+  """
+  def project(code, label) do
+    42
   end
 end
--- test/chorex_test.exs
+++ test/chorex_test.exs
@@ -1,8 +1,9 @@
 defmodule ChorexTest do
   use ExUnit.Case
   doctest Chorex
+  import Chorex
 
-  test "greets the world" do
-    assert Chorex.hello() == :world
+  test "smoke" do
+    assert ( defchor foo(Alice, Bob, Eve), do: "hello, world" ) == 42
   end
 end
