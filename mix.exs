3789620bff9f5c6e444803b8689dc7461f890fb8
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-05-30 16:28:19 -0600

    Add license, prepare for submission to Hex

--- mix.exs
+++ mix.exs
@@ -7,22 +7,32 @@ defmodule Chorex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Chorex",
+      source_url: "https://github.com/utahplt/chorex"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
+    []
+  end
+
+  defp description() do
+    "Chorex implements choreographic programming for Elixir with macros"
+  end
+
+  defp package() do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      name: "chorex",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/utahplt/chorex"}
     ]
   end
 end
