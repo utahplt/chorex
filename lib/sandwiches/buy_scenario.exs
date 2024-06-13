5d4c5a4fbcbc328f7b46c006bd49ac5c110393b1
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-06-13 17:10:30 -0600

    Working on the sandwiches scenario

---
+++ lib/sandwiches/buy_scenario.exs
@@ -0,0 +1,15 @@
+{:ok, store_proxy} = GenServer.start_link(StoreProxy, :go)
+
+customer = spawn(CustomerAlice, :init, [])
+:ok = GenServer.call(store_proxy, {:begin_session, [customer], 42})
+
+config = %{StoreProxy => store_proxy, Customer => customer, :super => self()}
+# Fake it from the customer so it gets the right session
+send(store_proxy, {:chorex, customer, {:config, config}})
+send(customer, {:config, config})
+
+receive do
+  m ->
+	IO.inspect(m, label: "got message")
+end
+
---
+++ lib/sandwiches/store_backend.ex
@@ -0,0 +1,45 @@
+defmodule StoreBackend do
+  @callback make_sandwich(any(), any()) :: any()
+  def init(impl) do
+    receive do
+      {:config, config} ->
+        ret = run_choreography(impl, config)
+        send(config[:super], {:choreography_return, StoreBackend, ret})
+    end
+  end
+
+  def big_chor(impl, config, sandwich_internals) do
+    bread =
+      receive do
+      msg -> msg
+    end
+
+    with ingredient_stack <- sandwich_internals.(impl, config, nil) do
+      send(config[Customer], impl.make_sandwich(bread, ingredient_stack))
+    end
+  end
+
+  def pbj(impl, config, _input_x) do
+    receive do
+      {:choice, Customer, L} ->
+        wash_hands =
+          receive do
+          msg -> msg
+        end
+
+      {:choice, Customer, R} ->
+        nil
+    end
+  end
+
+  def hamncheese(impl, config, _input_x) do
+  end
+
+  def run_choreography(impl, config) do
+    if function_exported?(impl, :run_choreography, 2) do
+      impl.run_choreography(impl, config)
+    else
+      big_chor(impl, config, &pbj/3)
+    end
+  end
+end
