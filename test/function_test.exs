929f969b4e32ce4e23cee1956ab94a56aad31266
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-07-16 16:00:58 -0600

    Cleanup

--- test/function_test.exs
+++ test/function_test.exs
@@ -29,23 +29,23 @@ defmodule FunctionTest do
     assert {_, _, _} = expanded # did we get something?
   end
 
-  quote do
-    defchor [CounterServer, CounterClient] do
-      def loop(CounterServer.(i)) do
-        if CounterClient.continue?() do
-          CounterClient[L] ~> CounterServer
-          CounterClient.bump() ~> CounterServer.(incr_amt)
-          loop(CounterServer.(incr_amt + i))
-        else
-          CounterClient[R] ~> CounterServer
-          CounterServer.(i) ~> CounterClient.(final_result)
-          CounterClient.(final_result)
-        end
-      end
+  # quote do
+  #   defchor [CounterServer, CounterClient] do
+  #     def loop(CounterServer.(i)) do
+  #       if CounterClient.continue?() do
+  #         CounterClient[L] ~> CounterServer
+  #         CounterClient.bump() ~> CounterServer.(incr_amt)
+  #         loop(CounterServer.(incr_amt + i))
+  #       else
+  #         CounterClient[R] ~> CounterServer
+  #         CounterServer.(i) ~> CounterClient.(final_result)
+  #         CounterClient.(final_result)
+  #       end
+  #     end
 
-      loop(CounterServer.(0))
-    end
-  end
+  #     loop(CounterServer.(0))
+  #   end
+  # end
   # |> Macro.expand_once(__ENV__)
   # |> Macro.to_string()
   # |> IO.puts()
