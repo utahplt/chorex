d1452e9725c830da1e9c8a35468a332e141c4c5f
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-07-31 17:30:59 -0600

    WIP init_func_test converted but not compiling

--- test/function_test.exs
+++ test/function_test.exs
@@ -34,27 +34,6 @@ defmodule FunctionTest do
     assert {_, _, _} = expanded
   end
 
-  # quote do
-  #   defchor [CounterServer, CounterClient] do
-  #     def loop(CounterServer.(i)) do
-  #       if CounterClient.continue?() do
-  #         CounterClient[L] ~> CounterServer
-  #         CounterClient.bump() ~> CounterServer.(incr_amt)
-  #         loop(CounterServer.(incr_amt + i))
-  #       else
-  #         CounterClient[R] ~> CounterServer
-  #         CounterServer.(i) ~> CounterClient.(final_result)
-  #         CounterClient.(final_result)
-  #       end
-  #     end
-
-  #     loop(CounterServer.(0))
-  #   end
-  # end
-  # |> Macro.expand_once(__ENV__)
-  # |> Macro.to_string()
-  # |> IO.puts()
-
   defmodule CounterTest do
     defchor [CounterServer, CounterClient] do
       def loop(CounterServer.(i)) do
