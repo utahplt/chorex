94790fc690c5ad0a3999fe0c5160200e64d54cf3
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-08-01 15:40:15 -0600

    Release v0.4.0 with bug fixes and support for arbitrary-arity funcs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chorex.MixProject do
   def project do
     [
       app: :chorex,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       description: description(),
