844ca07dae3931336e2647ae245c2e83933bf12a
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-10-24 16:40:06 -0600

    Cleanup old TCP modules

--- lib/chorex/tcp/handler_impl.ex
+++
@@ -1,7 +0,0 @@
-defmodule Chorex.Tcp.HandlerImpl do
-  use Chorex.Tcp.HandlerChor.Chorex, :handler
-
-  def process(msg, config) do
-    :ok
-  end
-end
--- lib/chorex/transport/backend.ex
+++
@@ -1,4 +0,0 @@
-defprotocol Chorex.Transport.Backend do
-  @spec send_msg(t, any()) :: [any()]
-  def send_msg(t, msg)
-end
