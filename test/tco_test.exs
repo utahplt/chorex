fe78be3bd049cd591c8e835fda5d6cee42767226
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-12-31 16:56:35 -0700

    Comment out noisy unused test; 39/42 tests passing

--- test/tco_test.exs
+++ test/tco_test.exs
@@ -2,26 +2,26 @@ defmodule TcoTest do
   use ExUnit.Case
   import Chorex
 
-  quote do
-    defchor [CounterServer, CounterClient] do
-      def loop(CounterServer.(i)) do
-        if CounterClient.continue?() do
-          CounterClient[L] ~> CounterServer
-          CounterClient.bump() ~> CounterServer.(incr_amt)
-          loop(CounterServer.(incr_amt + i))
-        else
-          CounterClient[R] ~> CounterServer
-          CounterServer.(i) ~> CounterClient.(final_result)
-          CounterClient.(final_result)
-        end
-      end
+  # quote do
+  #   defchor [CounterServer, CounterClient] do
+  #     def loop(CounterServer.(i)) do
+  #       if CounterClient.continue?() do
+  #         CounterClient[L] ~> CounterServer
+  #         CounterClient.bump() ~> CounterServer.(incr_amt)
+  #         loop(CounterServer.(incr_amt + i))
+  #       else
+  #         CounterClient[R] ~> CounterServer
+  #         CounterServer.(i) ~> CounterClient.(final_result)
+  #         CounterClient.(final_result)
+  #       end
+  #     end
 
-      def run() do
-        loop(CounterServer.(0))
-      end
-    end
-  end
-  |> Macro.expand_once(__ENV__)
-  |> Macro.to_string()
-  |> IO.puts()
+  #     def run() do
+  #       loop(CounterServer.(0))
+  #     end
+  #   end
+  # end
+  # |> Macro.expand_once(__ENV__)
+  # |> Macro.to_string()
+  # |> IO.puts()
 end
