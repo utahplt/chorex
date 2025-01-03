a414f3afb8057933adc038062c8a6110ce3be486
Ashton Wiersdorf <mail@wiersdorf.dev>
2025-01-03 15:55:17 -0700

    Working on new test for out-of-order choreographies

---
+++ test/ooo_tmp_test.exs
@@ -0,0 +1,196 @@
+defmodule OooTmpTest do
+  use ExUnit.Case
+
+  defmodule Ooo do
+    defmodule Chorex do
+      def get_actors() do
+        [Alice, Bob]
+      end
+
+      def alice do
+        quote do
+          alias OooTmpTest.Ooo.Chorex.Alice
+          alias OooTmpTest.Ooo.Chorex.Bob
+          import Alice
+          @behaviour Alice
+          use GenServer
+          defdelegate handle_continue(a, b), to: Alice
+
+          def init({parent_pid, args}) do
+            Alice.init(__MODULE__, parent_pid, args)
+          end
+        end
+      end
+
+      defmodule Alice do
+        @callback one() :: any()
+        @callback two() :: any()
+        def init(impl, parent_pid, args) do
+          state = %{
+            impl: impl,
+            vars: %{},
+            config: nil,
+            stack: [finish_choreography: %{return_pid: parent_pid}]
+          }
+
+          {:ok, state, {:continue, {:startup, args}}}
+        end
+
+        def handle_continue({:startup, args}, state) do
+          receive do
+            {:chorex, _session_token, :meta, {:config, config}} ->
+              apply(__MODULE__, :run, args ++ [%{state | config: config}])
+          end
+        end
+
+        def handle_continue({:finish_choreography, ret}, state) do
+          send(state.vars.return_pid, {:chorex_return, Alice, ret})
+          {:stop, :normal, state}
+        end
+
+        def run(state) do
+          ret = nil
+          config = state[:config]
+          impl = state[:impl]
+          :deferring_to_body
+
+          (
+          tok = config[:session_token]
+            send(config[Bob], {:chorex, tok, 1, Alice, Bob, impl.one()})
+          tok = config[:session_token]
+            send(config[Bob], {:chorex, tok, 2, Alice, Bob, impl.two()})
+          :here_return
+          state = put_in(state[:config], config)
+          state = put_in(state[:impl], impl)
+          :making_continue
+          [{tok, vars} | rest_stack] = state.stack
+          {:noreply, %{state | vars: vars, stack: rest_stack}, {:continue, {tok, ret}}}
+          )
+        end
+      end
+
+      def bob do
+        quote do
+          alias OooTmpTest.Ooo.Chorex.Bob
+          alias OooTmpTest.Ooo.Chorex.Alice
+          import Bob
+          nil
+          use GenServer
+          defdelegate handle_info(a, b), to: Bob
+          defdelegate handle_continue(a, b), to: Bob
+
+          def init({parent_pid, args}) do
+            Bob.init(__MODULE__, parent_pid, args)
+          end
+        end
+      end
+
+      defmodule Bob do
+        def init(impl, parent_pid, args) do
+          state = %{
+            impl: impl,
+            vars: %{},
+            config: nil,
+            stack: [finish_choreography: %{return_pid: parent_pid}]
+          }
+
+          {:ok, state, {:continue, {:startup, args}}}
+        end
+
+        def handle_continue({:startup, args}, state) do
+          receive do
+            {:chorex, _session_token, :meta, {:config, config}} ->
+              apply(__MODULE__, :run, args ++ [%{state | config: config}])
+          end
+        end
+
+        def handle_continue({:finish_choreography, ret}, state) do
+          send(state.vars.return_pid, {:chorex_return, Bob, ret})
+          {:stop, :normal, state}
+        end
+
+        def handle_info({:chorex, tok, 2, _, _, msg}, state)
+            when state.config.session_token == tok do
+          x = state.vars[:x]
+          config = state[:config]
+          impl = state[:impl]
+          y = msg
+
+          (
+          ret = x + y
+          :need_to_return
+          :here_return
+          state = put_in(state.vars[:y], y)
+          state = put_in(state.vars[:x], x)
+          state = put_in(state[:config], config)
+          state = put_in(state[:impl], impl)
+          :making_continue
+          [{tok, vars} | rest_stack] = state.stack
+          {:noreply, %{state | vars: vars, stack: rest_stack}, {:continue, {tok, ret}}}
+          )
+        end
+
+        def handle_info({:chorex, tok, 1, _, _, msg}, state)
+            when state.config.session_token == tok do
+          config = state[:config]
+          impl = state[:impl]
+          x = msg
+
+          (
+          :going_to_receive_see_handle_info
+          state = put_in(state.vars[:x], x)
+          state = put_in(state[:config], config)
+          state = put_in(state[:impl], impl)
+          {:noreply, state}
+          )
+        end
+
+        def handle_info(msg, state) do
+          dbg(state)
+          dbg(msg)
+          {:noreply, state}
+        end
+
+        def run(state) do
+          config = state[:config]
+          impl = state[:impl]
+          :deferring_to_body
+
+          (
+          :going_to_receive_see_handle_info
+          state = put_in(state[:config], config)
+          state = put_in(state[:impl], impl)
+          {:noreply, state}
+          )
+        end
+      end
+
+      defmacro __using__(which) do
+        apply(__MODULE__, which, [])
+      end
+    end
+  end
+
+  defmodule MyAlice do
+    use Ooo.Chorex, :alice
+
+    def one(), do: 1
+    def two(), do: 2
+  end
+
+  defmodule MyBob do
+    use Ooo.Chorex, :bob
+  end
+
+  test "eh" do
+    Chorex.start(
+                Ooo.Chorex,
+                %{
+                  Alice => MyAlice,
+                  Bob => MyBob
+                },
+                [])
+
+    assert_receive {:chorex_return, OooTmpTest.Ooo.Chorex.Bob, 3}
+  end
+end
